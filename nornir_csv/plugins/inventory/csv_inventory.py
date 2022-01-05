52c06deb45c4112bb92701be1e3dede29898e740
Matheus Augusto da Silva <a.matheus26@hotmail.com>
2022-01-04 21:27:55 -0300

    Add _get_hosts method from CSV

--- nornir_csv/plugins/inventory/csv_inventory.py
+++ nornir_csv/plugins/inventory/csv_inventory.py
@@ -1,22 +1,37 @@
 """Reads CSV file as a Nornir inventory."""
+import os
 import csv
 from nornir.core.inventory import (Inventory,
                                    Hosts,
                                    Host,
                                    Groups,
                                    Group)
-from typing import Dict, Any
+from typing import List, Dict, Any
 
 class CsvInventory:
-    def __init__(self, inventory_dir_path: str = "./inventory/") -> None:
+    def __init__(self, inventory_dir_path: str = "./inventory") -> None:
         self.inventory_dir_path = inventory_dir_path
 
-    def _csv_to_dict(self):
-        pass
+    def _pop_dict_return(self,in_dict: Dict, key: str) -> Dict:
+        copied = in_dict.copy()
+        copied.pop(key)
+        return copied
 
-    def _hosts_dict_to_hostobject(self, hosts_dict: Dict[Any, Any]) -> Hosts:
-        print(hosts_dict)
-        return Hosts()
+    def _csv_to_dictlist(self, csv_file_name: str) -> List[Dict]:
+        """Return list of dictionaries with data from csv file."""
+        print(f"Opening file {csv_file_name}")
+        with open(csv_file_name,'r') as _f:
+            dict_reader = csv.DictReader(_f)
+            dict_list = list(dict_reader)
+        return dict_list
+
+    def _get_hosts(self) -> Hosts:
+        host_list = self._csv_to_dictlist(
+            os.path.join(self.inventory_dir_path,
+                         'hosts.csv'))
+        hosts_data = Hosts({
+            item['name'] : Host(**item) for item in host_list })
+        return Hosts(**hosts_data)
 
     def _groups_dict_to_groupobject(self, groups_dict: Dict[Any,Any]) -> Groups:
         print(groups_dict)
