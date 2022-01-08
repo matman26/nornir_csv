49bb444da1cf66427cfbab1227561b8ea310ec28
Matheus Augusto da Silva <a.matheus26@hotmail.com>
2022-01-08 19:18:18 -0300

    Published to PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'nornir_csv',
   packages = ['nornir_csv'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='apache-2.0',
   description = 'CSV Inventory for nornir with hosts, groups and defaults.',
-  long_description = open('README.md','r',encoding='utf-8').read(),
+  long_description = open('README','r',encoding='utf-8').read(),
   long_description_content_type="text/markdown",
   author = 'Matheus Augusto da Silva',
   author_email = 'a.matheus26@hotmail.com',
