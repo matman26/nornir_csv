ef895ddeb27226d1f020c2121bbc3e1f48ab8bb0
Matheus Augusto da Silva <a.matheus26@hotmail.com>
2022-01-08 18:44:10 -0300

    Release setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'nornir_csv',
+  packages = ['nornir_csv'],
+  version = '0.1',
+  license='apache-2.0',
+  description = 'CSV Inventory for nornir with hosts, groups and defaults.',
+  author = 'Matheus Augusto da Silva',
+  author_email = 'a.matheus26@hotmail.com',
+  url = 'https://github.com/matman26/nornir_csv',
+  download_url = 'https://github.com/matman26/nornir_csv/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['nornir', 'csv', 'inventory', 'plugin'],
+  install_requires=[
+          'nornir',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
