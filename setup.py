5cabaa7bf8f6af3b3903399bda91f513595cbb46
Matheus Augusto da Silva <a.matheus26@hotmail.com>
2022-01-22 22:44:53 -0300

    Setup.py for 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'nornir_csv',
   py_modules = ['nornir_csv.plugins.inventory.csv_inventory'],
-  version = '0.2.0',
+  version = '0.2.2',
   license='apache-2.0',
   description = 'CSV Inventory for nornir with hosts, groups and defaults.',
   long_description = open('README.md','r',encoding='utf-8').read(),
@@ -11,7 +11,7 @@ setup(
   author = 'Matheus Augusto da Silva',
   author_email = 'a.matheus26@hotmail.com',
   url = 'https://github.com/matman26/nornir_csv',
-  download_url = 'https://github.com/matman26/nornir_csv/archive/refs/tags/v0.2.tar.gz',
+  download_url = 'https://github.com/matman26/nornir_csv/archive/refs/tags/v0.2.2.tar.gz',
   keywords = ['nornir', 'csv', 'inventory', 'plugin'],
   install_requires=[
       'nornir',
@@ -22,8 +22,9 @@ setup(
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: Apache Software License',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
